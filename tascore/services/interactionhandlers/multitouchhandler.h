62c7304834e8c25c9239e60c6efef1d086e1ca52
Ari Hyttinen <ext-ari.hyttinen@nokia.com>
2010-09-30 15:44:04 +0300

    Fixed QAction trigger and hover.  Also removed actionhandler.*, did cleanup, added handlerName() method to InteractionHnadler.

--- tascore/services/interactionhandlers/multitouchhandler.h
+++ tascore/services/interactionhandlers/multitouchhandler.h
@@ -1,22 +1,22 @@
-/*************************************************************************** 
-** 
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
-** All rights reserved. 
-** Contact: Nokia Corporation (testabilitydriver@nokia.com) 
-** 
+/***************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (testabilitydriver@nokia.com)
+**
 ** This file is part of Testability Driver Qt Agent
-** 
-** If you have questions regarding the use of this file, please contact 
-** Nokia at testabilitydriver@nokia.com . 
-** 
-** This library is free software; you can redistribute it and/or 
-** modify it under the terms of the GNU Lesser General Public 
-** License version 2.1 as published by the Free Software Foundation 
-** and appearing in the file LICENSE.LGPL included in the packaging 
-** of this file. 
-** 
-****************************************************************************/ 
- 
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at testabilitydriver@nokia.com .
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
 
 
 #ifndef MULTITOUCHHANDLER_H
@@ -34,15 +34,16 @@ class MultitouchHandler : public InteractionHandler
 public:
     MultitouchHandler();
     ~MultitouchHandler();
-  
-	bool executeInteraction(TargetData data);
-	bool executeMultitouchInteraction(QList<TargetData> dataList);
+
+    virtual QString handlerName() { return QString("MultitouchHandler"); }
+    bool executeInteraction(TargetData data);
+    bool executeMultitouchInteraction(QList<TargetData> dataList);
 
 private:
-	QStringList mPressCommands;
-	QStringList mReleaseCommands;
-	TasGestureFactory* mFactory;
-	TasTouchEventGenerator mTouchGen;
+        QStringList mPressCommands;
+        QStringList mReleaseCommands;
+        TasGestureFactory* mFactory;
+        TasTouchEventGenerator mTouchGen;
 };
 
 #endif
