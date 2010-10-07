6b5bdef1c6ff8e6a6f75bfaf0296b41f4f7bcaa2
Tuukka Lahtela <ext-tuukka.m.lahtela@nokia.com>
2010-10-07 12:07:48 +0300

    Application uid parsed from command xml

--- tascore/corelib/tascommandparser.cpp
+++ tascore/corelib/tascommandparser.cpp
@@ -67,7 +67,7 @@ TasCommandModel* TasCommandParser::parseCommandXml(const QString& commandXml)
         model->setSourceString(commandXml);
         QDomElement root = doc.documentElement();
         model->setId(root.attribute("id"));
-        model->setUId(root.attribute("uid"));
+        model->setUId(root.attribute("applicationUid"));
         model->setName(root.attribute("name"));
         model->setService(root.attribute("service"));        
         if(root.attribute("async") == "true"){
