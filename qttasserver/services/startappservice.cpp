062c2540482b7d67c1e4aeec5a5ecc2599740c12
Ivan Castane Capel <ivacasta@.NOE.Nokia.com>
2011-02-02 12:57:35 +0000

    Commented out linux native code on startup for fix

--- qttasserver/services/startappservice.cpp
+++ qttasserver/services/startappservice.cpp
@@ -243,7 +243,7 @@ void StartAppService::launchDetached(const QString& applicationPath, const QStri
 
     }
 
-#elif (defined(Q_OS_UNIX) || defined(Q_OS_WS_MAC))
+#elif (defined(Q_OS_UNIX) && defined(Q_OS_WS_MAC) && defined(Q_OS_WIN32) )
 
     pid_t pid, sid, grandpid;
 
